ac248abebfdb2e569757b954ba503197443e4aad
cristiancano88 <cristiancano888@gmail.com>
2015-09-29 23:03:32 -0500

    Sección de saluda

---
+++ protected/controllers/SaludoController.php
@@ -0,0 +1,8 @@
+<?php
+class SaludoController extends Controller{
+	public function actionIndex(){
+	
+	$saludo = 'Hola Que tal como estas?';
+	$this->render('index',array('saludo'=>$saludo));
+	}
+}
